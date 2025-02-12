d658ff09b70331d7e7fd1fd1af2b0fbcda43cf25
phkha <phkha>
2025-02-12 03:01:28 +0000

    remove modal component and styles; simplify landing page layout

--- nextjs-app/src/app/(guest)/landingpage/page.tsx
+++ nextjs-app/src/app/(guest)/landingpage/page.tsx
@@ -1,18 +1,7 @@
-"use client"
-import Modal from "@/lib/ui/modal/modal";
-import { useState } from "react";
-
 export default function Dashboard() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
-      <div>
-        <button onClick={() => setShowModal(true)}>Open Modal</button>
-        {showModal &&
-            <Modal onClose={() => setShowModal(false)}>
-                Hello from the modal!
-            </Modal>
-        }
-      </div>
+    <div>
+      Hello from the modal!
+    </div>
   )
 }
